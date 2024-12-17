340e117fcdfbcf37c92deb7be3a642d23b96fcd9
Kit <b@yorkemail.org>
2024-12-18 10:17:41 +1300

    make the ScrewMilk mixin actually good

--- src/main/java/kit/nova_arcana/mixin/ScrewMilk.java
+++ src/main/java/kit/nova_arcana/mixin/ScrewMilk.java
@@ -2,47 +2,30 @@ package kit.nova_arcana.mixin;
 
 
 import kit.nova_arcana.ModEffects;
-import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.item.MilkBucketItem;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.stat.Stats;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MilkBucketItem.class)
-public abstract class ScrewMilk {
-    /**
-     * @author
-     * @reason
-     */
-    @Overwrite
-    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-        if (user instanceof ServerPlayerEntity serverPlayerEntity) {
-            Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
-            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat((MilkBucketItem)((Object)this)));
+public abstract class ScrewMilk extends Item {
+    public ScrewMilk(Settings settings) {
+        super(settings);
+    }
+    @Inject(
+            at = @At("HEAD"),
+            method = "finishUsing(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;)Lnet/minecraft/item/ItemStack;",
+            cancellable = true
+    )
+    public void finishUsing(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
+        if (user.getStatusEffect(ModEffects.INSTANCE.getFAIR_GROUND()) != null) {
+            cir.setReturnValue(stack);
+            cir.cancel();
         }
-
-        if (user instanceof PlayerEntity && !((PlayerEntity)user).getAbilities().creativeMode) {
-            stack.decrement(1);
-        }
-
-        if (!world.isClient) {
-            if (user.getStatusEffect(ModEffects.INSTANCE.getFAIR_GROUND()) == null) {
-                user.clearStatusEffects();
-            }
-        }
-
-        return stack.isEmpty() ? new ItemStack(Items.BUCKET) : stack;
     }
 }
